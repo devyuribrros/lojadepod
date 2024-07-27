f9469f49e0ea560aacefa3a7436f67f9efa8063a
devyuribrros <yuribrrosdev@gmail.com>
2024-07-27 14:59:14 -0300

    Add files via upload

---
+++ PROJETO MAT/js/dados.js
@@ -0,0 +1,219 @@
+var MENU = {
+    "churrasco": [
+        {
+            "id": "005-kings-carolina-oink-sampler",
+            "img": "./img/cardapio/churrasco/pod2.jpg",
+            "name": "Strawberry watermelon",
+            "dsc": "Carolina BBQ Oink Sampler",
+            "price": 90
+        },
+        {
+            "id": "texas-monthlys-1-bbq-brisket",
+            "img": "./img/cardapio/churrasco/pod4.jpg",
+            "name": "Green Apple",
+            "dsc": "Texas Monthly's #1 BBQ Brisket",
+            "price": 90
+        },
+        {
+            "id": "whole-brisket-texas-bbq-sauce",
+            "img": "./img/cardapio/churrasco/pod3.jpg",
+            "name": "Menthol",
+            "dsc": "Whole Brisket + Texas Barbecue BBQ Sauce",
+            "price": 90
+        },
+       
+    ],
+    "burgers": [
+        {
+            "id": "the-gramercy-tavern-burger-4-pack",
+            "img": "./img/cardapio/burguers/pod2.jpg",
+            "name": "Strawberry watermelon",
+            "dsc": "The Gramercy Tavern Burger - 4 Pack",
+            "price": 90
+        },
+        {
+            "id": "shake-shack-shackburger-8-pack",
+            "img": "./img/cardapio/burguers/pod4.jpg",
+            "name": "Green Apple",
+            "dsc": "Shake Shack ShackBurger® - 8 Pack",
+            "price": 90
+        },
+        {
+            "id": "gotts-cheeseburger-kit-for-4",
+            "img": "./img/cardapio/burguers/pod3.jpg",
+            "name": "Menthol",
+            "dsc": "Gott's Complete Cheeseburger Kit for 4",
+            "price": 90
+        },
+        {
+            "id": "le-big-matt-kit-for-6",
+            "img": "./img/cardapio/burguers/pod5.jpg",
+            "name": "Ice minty",
+            "dsc": "Le Big Matt Burger Kit for 6",
+            "price": 115
+        },
+        {
+            "id": "shake-shack-shackburger-16-pack",
+            "img": "./img/cardapio/burguers/pod5.jpg",
+            "name": "Grape ice",
+            "dsc": "Shake Shack Shackburger® - 16 Pack",
+            "price": 115
+        },
+        {
+            "id": "21-usda-prime-burgers-pack-of-18-8oz-each",
+            "img": "./img/cardapio/burguers/pod5.jpg",
+            "name": "Watermelon ice",
+            "dsc": "USDA Prime Burgers - Pack of 18 (8oz each)",
+            "price": 115
+        },
+        {
+            "id": "goldbelly-burger-bash-pack",
+            "img": "./img/cardapio/burguers/pod5.jpg",
+            "name": "Green Apple",
+            "dsc": "Goldbelly \"Burger Bash\" Pack",
+            "price": 115
+        },
+        {
+            "id": "burger-au-poivre-kit-4-pack",
+            "img": "./img/cardapio/burguers/pod5.jpg",
+            "name": "Strawberry kiwi",
+            "dsc": "Burger Au Poivre Kit - 4 Pack",
+            "price": 115
+        },
+        {
+            "id": "gotts-complete-cheeseburger-kit-for-8",
+            "img": "./img/cardapio/burguers/pod5.jpg",
+            "name": "Strawberry ice",
+            "dsc": "Gott's Complete Cheeseburger Kit for 8",
+            "price": 115
+        },
+        {
+            "id": "gramercy-tavern-burger-kielbasa-combo",
+            "img": "./img/cardapio/burguers/pod5.jpg",
+            "name": "Banana ice",
+            "dsc": "Gramercy Tavern Burger + Kielbasa Combo",
+            "price": 115
+        },
+        {
+            "id": "goldbelly-burger-blend-4-lbs",
+            "img": "./img/cardapio/burguers/pod11.jfif",
+            "name": "Flannery Beef",
+            "dsc": "Goldbelly Burger Blend - 4 lbs.",
+            "price": 115
+        },
+        {
+            "id": "double-stack-burger-kit-for-4",
+            "img": "./img/cardapio/burguers/pod22.jfif",
+            "name": "Holeman & Finch",
+            "dsc": "Double Stack Burger Kit for 4",
+            "price": 115
+        },
+        
+    ],
+    
+    "sobremesas": [
+        {
+            "id": "15259-german-chocolate-killer-brownie-tin-pack",
+            "img": "./img/cardapio/sobremesas/pod18.jfif",
+            "name": "Melancia Morango",
+            "dsc": "German Chocolate Killer Brownie®",
+            "price": 110
+        },
+        {
+            "id": "jacques-world-famous-chocolate-chip-cookies",
+            "img": "./img/cardapio/sobremesas/pod17.jfif",
+            "name": "Menta refrescante",
+            "dsc": "Jacques' World Famous Chocolate Chip Cookies - 6 Pack",
+            "price": 110
+        },
+       
+    ],
+    "bebidas": [
+        {
+            "id": "hong-kong-boba-tea-kit-for-6",
+            "img": "./img/cardapio/bebidas/pod22.jfif",
+            "name": "Strawberry Watermelon ",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 115
+        },
+        {
+            "id": "guys-caliente-margaritas-for-12",
+            "img": "./img/cardapio/bebidas/pod23.jfif",
+            "name": "Clear",
+            "dsc": "Guy's Caliente Margaritas for 12",
+            "price": 115
+        },
+        
+    ],
+    "pizzas": [
+        {
+            "id": "23699-choose-your-own-thin-crust-pizza-4-pack",
+            "img": "./img/cardapio/pizzas/pod5.jpg",
+            "name": "Ice minty",
+            "dsc": "Choose Your Own Thin Crust Pizza - 4 Pack",
+            "price": 115
+        },
+        {
+            "id": "choose-your-own-new-haven-style-pizza-6-pack",
+            "img": "./img/cardapio/pizzas/pod5.jpg",
+            "name": "Grape ice",
+            "dsc": "New Haven-Style Pizza - 6 Pack (Choose Your Own)",
+            "price": 115
+        },
+        {
+            "id": "6-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/cardapio/pizzas/pod5.jpg",
+            "name": "Watermelon ice",
+            "dsc": "6 Lou Malnati's Deep Dish Pizzas",
+            "price": 115
+        },
+        {
+            "id": "plain-thin-crust-pizza-4-pack",
+            "img": "./img/cardapio/pizzas/pod5.jpg",
+            "name": "Green Apple",
+            "dsc": "Plain Thin Crust Pizza - 4 Pack",
+            "price": 115
+        },
+        {
+            "id": "wood-fired-pizzas-best-seller-4-pack",
+            "img": "./img/cardapio/pizzas/pod5.jpg",
+            "name": "Strawberry kiwi",
+            "dsc": "Wood Fired Pizzas Best Seller - 4 Pack",
+            "price": 115
+        },
+        {
+            "id": "236991-choose-your-own-deep-dish-pizza-3-pack",
+            "img": "./img/cardapio/pizzas/pod5.jpg",
+            "name": "Banana ice",
+            "dsc": "Choose Your Own Deep Dish Pizza - 3 Pack",
+            "price": 115
+        },
+        {
+            "id": "choose-your-own-detroit-style-pizza-3-pack",
+            "img": "./img/cardapio/pizzas/pod5.jpg",
+            "name": "Strawberry ice",
+            "dsc": "Detroit-Style Pizza - Choose Your Own 3 Pack",
+            "price": 115
+        },
+        
+      
+    ],
+    "steaks": [
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./img/cardapio/steaks/pod15.jfif",
+            "name": "Strawberry Watermelon",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 115
+        },
+        {
+            "id": "steaks-and-cakes-date-night-dinner-for-2",
+            "img": "./img/cardapio/steaks/pod12.jfif",
+            "name": "Sakura grape",
+            "dsc": "Steaks and Cakes Date Night Dinner for 2",
+            "price": 115
+        }
+        
+        
+    ]
+}
